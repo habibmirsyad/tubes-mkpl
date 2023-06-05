027208c88c14eb3d0b095a578b09c1a12cb35564
Aditya MP <211wadityaw112@gmail.com>
2023-06-05 08:57:28 +0700

    add game

--- src/main/java/id/my/admfirst/tubesmkpl/App.java
+++ src/main/java/id/my/admfirst/tubesmkpl/App.java
@@ -1,13 +1,36 @@
 package id.my.admfirst.tubesmkpl;
+import java.util.Scanner;
 
 /**
- * Hello world!
- *
+ * Dice Game App!
+ * 
  */
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Welcome to the Dice Game!");
+        System.out.println("Enter the target score: ");
+        int targetScore = scanner.nextInt();
+
+        DiceGame game = new DiceGame(targetScore);
+
+        while (!game.isGameOver()) {
+            game.rollDice();
+
+            if (!game.isGameOver()) {
+                System.out.println("Do you want to roll again? (Y/N)");
+                String choice = scanner.next();
+
+                if (choice.equalsIgnoreCase("N")) {
+                    System.out.println("Game over. You did not reach the target score.");
+                    game.setGameOver(true);
+                }
+            }
+        }
+
+        scanner.close();
     }
 }
---
+++ src/main/java/id/my/admfirst/tubesmkpl/DiceGame.java
@@ -0,0 +1,39 @@
+package id.my.admfirst.tubesmkpl;
+
+import java.util.Random;
+
+public class DiceGame {
+    private static Random random = new Random();
+
+    private int targetScore;
+    private int totalScore;
+    private boolean gameOver;
+
+    public DiceGame(int targetScore) {
+        this.targetScore = targetScore;
+        this.totalScore = 0;
+        this.gameOver = false;
+    }
+
+    public void rollDice() {
+        int diceRoll = random.nextInt(6) + 1;
+        System.out.println("You rolled a " + diceRoll);
+
+        totalScore += diceRoll;
+
+        if (totalScore >= targetScore) {
+            System.out.println("Congratulations! You reached the target score of " + targetScore);
+            gameOver = true;
+        } else {
+            System.out.println("Your current score is: " + totalScore);
+        }
+    }
+
+    public boolean isGameOver() {
+        return gameOver;
+    }
+
+    public void setGameOver(boolean gameOver){
+        this.gameOver = gameOver;
+    }
+}
